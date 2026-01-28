bd1b6a93a3bdad4dce4736260cc299a3f2500c60
Artem Lipatkin <artemon-lipaboy@ya.ru>
2026-01-28 16:55:00 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-while true; do py play_random_song.py; sleep $(($RANDOM%100)); [ $(($RANDOM%100)) ] && break; done
+while true; do python play_random_song.py; sleep $(($RANDOM%100)); [ $(($RANDOM%100)) ] && break; done
