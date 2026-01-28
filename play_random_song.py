6ff1f14dd6d26b51dadc01a2609721e8e4261486
Artem Lipatkin <artemon-lipaboy@ya.ru>
2026-01-28 11:04:34 +0700

    Update play_random_song.py

--- play_random_song.py
+++ play_random_song.py
@@ -81,9 +81,8 @@ if __name__ == "__main__":
         song_audio_segment : AudioSegment = AudioSegment.from_mp3(LOCAL_SONG_PATH)
 
         if not through_proxy:
-            print('mpv')
-            subprocess.run(f'mpv {LOCAL_SONG_PATH} &> /dev/null', shell=True)
-            print('end')
+            FNULL = open(os.devnull, 'w')
+            subprocess.run(f'mpv {LOCAL_SONG_PATH} &> /dev/null', stdout=FNULL, shell=True)
             # os.remove(LOCAL_SONG_PATH)
         else:
             os.remove(LOCAL_SONG_PATH)
@@ -91,9 +90,3 @@ if __name__ == "__main__":
     except Exception as e:
         print(e)
         
-
-
-
-
-
-
