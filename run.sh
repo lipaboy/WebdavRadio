705d6afc109d698bd3c25255de12a0c3a47be78b
Artem <artemon-lipaboy@ya.ru>
2026-02-12 11:36:13 +0700

    run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-while true; do python play_random_song.py; sleep $(($RANDOM%100)); [ $(($RANDOM%100)) ] && break; done
+while true; do python $(dirname -- ${BASH_SOURCE})/play_random_song.py; sleep $(($RANDOM%100)); [ $(($RANDOM%100)) -ge 80 ] && break; done
